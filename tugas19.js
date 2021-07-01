180f96a279da3bd24896f8c33e5c66ea0f6019b3
Decky Fajar Sidiq <46802206+deckyfajars@users.noreply.github.com>
2021-07-01 17:03:36 +0700

    Add files via upload

---
+++ tugas19.js
@@ -0,0 +1,17 @@
+function ulang(){
+  var kata = "Semangat...";
+  console.log(kata.repeat(5));
+
+  var kata2 = "*";
+  console.log(kata2.repeat(5));
+  console.log(kata2.repeat(10));
+  console.log(kata2.repeat(30));
+  console.log(kata2.repeat(50));
+  console.log(kata2.repeat(70));
+  console.log(kata2.repeat(50));
+  console.log(kata2.repeat(30));
+  console.log(kata2.repeat(10));
+  console.log(kata2.repeat(5));
+}
+
+ulang();
